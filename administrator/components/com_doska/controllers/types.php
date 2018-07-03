0288fda8977b9665eaf1aa4c05211a08bde92acc
Dmitry <zatulenko@gmail.com>
2018-07-04 00:54:31 +0300

    Добавлен контроллер types

---
+++ administrator/components/com_doska/controllers/types.php
@@ -0,0 +1,13 @@
+<?php
+defined("_JEXEC") or die();
+
+class DoskaControllerTypes extends JControllerAdmin
+{
+
+    public function getModel($name = 'Type', $prefix = 'DoskaModel', $config = array())
+    {
+
+        return parent::getModel($name, $prefix, $config);
+    }
+}
+?>
