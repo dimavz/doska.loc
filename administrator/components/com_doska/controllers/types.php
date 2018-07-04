3ab6b74060bb5352d682ead57735612d27b0b707
Dmitry <zatulenko@gmail.com>
2018-07-04 12:43:31 +0300

    Реализован функционал изменения типов объявлений

--- administrator/components/com_doska/controllers/types.php
+++ administrator/components/com_doska/controllers/types.php
@@ -6,7 +6,6 @@ class DoskaControllerTypes extends JControllerAdmin
 
     public function getModel($name = 'Type', $prefix = 'DoskaModel', $config = array())
     {
-
         return parent::getModel($name, $prefix, $config);
     }
 }
--- administrator/components/com_doska/tables/type.php
+++ administrator/components/com_doska/tables/type.php
@@ -11,4 +11,27 @@ class DoskaTableType extends JTable
     public function __construct($db) {
         parent::__construct('#__doska_types','id',$db);
     }
+
+    public function publish($pks = null, $state = 1, $userId =0){
+        JArrayHelper::toInteger($pks);
+        $state = (int)$state;
+        if (empty($pks)){
+            throw new RuntimeException(JText::_('JLIB_DATABASE_ERROR_NO_ROWS_SELECTED'));
+        }
+//        print_r($pks);
+//        exit();
+
+        foreach ($pks as $pk){
+            if (!$this->load($pk)){
+                throw new RuntimeException(JText::_('COM_DOSKA_TABLE_ERROR_TYPE'));
+            }
+//            print_r($this);
+//            exit();
+            $this->state = $state;
+            if(!$this->store()){
+                throw new RuntimeException(JText::_('COM_DOSKA_TABLE_ERROR_TYPE_STORE'));
+            }
+        }
+        return true;
+    }
 }
