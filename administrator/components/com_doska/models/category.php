d2e1941226e328e3ae5da8ca884f53778fd76cfb
Dmitry <zatulenko@gmail.com>
2018-07-05 16:13:45 +0300

    Создана модель категории объекта

---
+++ administrator/components/com_doska/models/category.php
@@ -0,0 +1,62 @@
+<?php
+defined("_JEXEC") or die();
+
+class DoskaModelCategory extends JModelAdmin {
+
+    public function getForm($data = array(),$loadData = true) {
+
+
+        $form = $this->loadForm(
+            $this->option.'_category',
+            'category',
+            array('control'=>'jform','load_data'=>$loadData)
+        );
+
+
+        if(empty($form)) {
+            return FALSE;
+        }
+
+        return $form;
+    }
+
+    public function getTable($type = 'Category', $prefix = 'DoskaTable',$config = array()) {
+        $table = JTable::getInstance($type,$prefix,$config);
+        return $table;
+    }
+
+    protected function loadFormData()
+    {
+        // Вытаскиваем данные из сесии
+        $data = JFactory::getApplication()->getUserState('com_doska.edit.category.data',array());
+
+        if(empty($data)) {
+            $data = $this->getItem();
+        }
+
+        return $data;
+    }
+
+    public function save ($data){
+//        print_r($data);
+//        exit();
+        if(!trim($data['name'])) {
+            $this->setError(JText::_('COM_DOSKA_WARNING_PROVIDE_VALID_NAME'));
+            return FALSE;
+        }
+
+        if(trim($data['alias']) == '') {
+            $data['alias'] = $data['name'];
+            $data['alias'] = JApplicationHelper::stringURLSafe($data['alias']);
+        }
+        else{
+            $data['alias'] = JApplicationHelper::stringURLSafe($data['alias']);
+        }
+
+        if(parent::save($data)) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+}
+?>
