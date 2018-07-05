bd958d2066913db8b15a1b4c1f4f0846819fa84a
Dmitry <zatulenko@gmail.com>
2018-07-05 16:21:57 +0300

    Зарегистрирован класс DoskaHelper

--- administrator/components/com_doska/doska.php
+++ administrator/components/com_doska/doska.php
@@ -3,6 +3,14 @@ defined('_JEXEC') or die('Restricted Access');
 
 $controller = JControllerLegacy::getInstance('Doska');//DoskaController
 // $controller->registerTask('hello','h'); // регистрация задачи в контроллере
+
+//Регистрация класса DoskaHelper
+JLoader::register('DoskaHelper',__DIR__.'/helpers/doska.php');
+
+//echo "<pre>";
+//print_r(JLoader::getClassList());
+//echo "</pre>";
+
 $input = jFactory::getApplication()->input; // Получаем объект, который содержит переменные строки запроса
 $controller->execute($input->get('task','display'));
 $controller->redirect();
