5937f0ad4aa73109cca2dc74f6828359347028a8
Dmitry <zatulenko@gmail.com>
2018-07-09 10:59:05 +0300

    Исключено из вывода поля категорий подчинённое поле

--- administrator/components/com_doska/models/fields/categoryparent.php
+++ administrator/components/com_doska/models/fields/categoryparent.php
@@ -6,56 +6,63 @@ JFormHelper::loadFieldClass('list'); // Загрузка класса JFormField
 class JFormFieldCategoryparent extends JFormFieldList
 {
     protected $type = 'Categoryparent';
+
+    protected $parent = null;
+
     protected function getOptions()
     {
         //Получаем id редактируемой категории
         $edit_id = null;
-        $input = jFactory::getApplication()->input;
+        $input = JFactory::getApplication()->input;
         $edit_id = $input->get('id');
-//
+
+
         $rows = array();
         $options = array();
 
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
 
-        $query->select($db->quoteName(array('id','name', 'parentid'), array('value','text','parentid')));
+        $query->select($db->quoteName(array('id', 'name', 'parentid'), array('value', 'text', 'parentid')));
         $query->from($db->quoteName('#__doska_categories'));
-        $query->where($db->quoteName('state').' = '. $db->quote(1));
+        $query->where($db->quoteName('state') . ' = ' . $db->quote(1));
 
         $db->setQuery($query);
-        try{
+        try {
             $rows = $db->loadObjectList();
-        }
-        catch(RuntimeException $e)
-        {
-            JError::raiseWarning(500,$e->getMessage());
+        } catch (RuntimeException $e) {
+            JError::raiseWarning(500, $e->getMessage());
         }
 //        print_r($rows);
         $parent = new stdClass();
         $parent->text = JText::_('JGLOBAL_ROOT_PARENT');
         $parent->value = 0;
 
-        array_push($options,$parent);
+        array_push($options, $parent);
 
-        if ($edit_id != null){
-            if(!empty($rows)){
-                foreach ($rows as $row)
-                {
-                    if($row->parentid == 0 && $row->value != $edit_id ){
-                        array_push($options,$row);
+        if ($edit_id != null) {
+            if (!empty($rows)) {
+                foreach ($rows as $row) {
+                    if ($row->value == $edit_id) {
+                        $this->parent = $row->value;
+                        break;
                     }
                 }
+                foreach ($rows as $row) {
+                    if (($row->value != $edit_id && $row->parentid != $this->parent)&& $row->parentid == 0) {
+
+                        array_push($options, $row);
+
+                    }
+                }
+
             }
             return $options;
-        }
-        else
-        {
-            if(!empty($rows)){
-                foreach ($rows as $row)
-                {
-                    if($row->parentid == 0 ){
-                        array_push($options,$row);
+        } else {
+            if (!empty($rows)) {
+                foreach ($rows as $row) {
+                    if ($row->parentid == 0) {
+                        array_push($options, $row);
                     }
                 }
             }
